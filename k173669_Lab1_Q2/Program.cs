bcdc7489b44384eca23c172ffee55e1e4c3cc4a7
mbilalakmal <37498621+mbilalakmal@users.noreply.github.com>
2021-02-24 23:53:49 +0500

    Added Q2

---
+++ k173669_Lab1_Q2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace k173669_Lab1_Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string rawInput = Console.ReadLine();
+
+            /// check if successfully parsed
+            if (string.IsNullOrWhiteSpace(rawInput))
+            {
+                throw new ApplicationException("Please enter a valid string.");
+            }
+
+            string answer;
+
+            if (rawInput.Length < 5)
+            {
+                answer = rawInput.ToLower();
+            }
+            else
+            {
+                answer = ToLowerFourAndUpperRest(rawInput);
+            }
+
+            Console.WriteLine(answer);
+
+            /// Wait for user input
+            Console.ReadKey(true);
+
+        }
+
+        static string ToLowerFourAndUpperRest(string input)
+        {
+            string startingFour = input.Substring(0, 4).ToLower();
+            string rest = input.ToUpper();
+
+            string result = startingFour + rest.Substring(4);
+
+            return result;
+        }
+    }
+}
